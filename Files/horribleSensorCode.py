d9e272fa7b7d21648cc6ab399a81da5f9f08fdae
Ian Novotne <inovotn04@ccs.local>
2021-09-20 09:50:23 -0400

    AHHHHHHH

--- Files/horribleSensorCode.py
+++ Files/horribleSensorCode.py
@@ -2,15 +2,27 @@ import time
 import board
 import adafruit_hcsr04
 import neopixel
+import simpleio
+
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+AHHH = sonar.distance
 
 while True:
+    print("well SOMETHING is running")
     try:
         print((sonar.distance,))
+        AHHH = sonar.distance
     except RuntimeError:
         print("N/A")
-    if distance>=5 and distance<20:
-        blue=simpleio.map_range(distance,5,20,0,255)
-    if distance>=20 and distance<35:
+    if AHHH >= 5 and AHHH < 20:
+        red = simpleio.map_range(distance, 5, 20, 255, 0)
+        blue = simpleio.map_range(distance, 5, 20, 0, 255)
+        green = 0
+    if AHHH >= 20 and AHHH < 35:
+        red = 0
+        blue = simpleio.map_range(distance, 5, 20, 255, 0)
+        green = simpleio.map_range(distance, 5, 20, 0, 255)
+    dot.fill(red, blue, green)
 
     time.sleep(0.1)
