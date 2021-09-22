1c7da97d5f081f9176adf71c93f34395e1497890
Ian Novotne <inovotn04@ccs.local>
2021-09-22 10:10:47 -0400

    adding FINALIZED code for the distance sensor

--- Files/horribleSensorCode.py
+++ Files/horribleSensorCode.py
@@ -6,23 +6,22 @@ import simpleio
 
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-AHHH = sonar.distance
+dot.brightness = 0.1
 
 while True:
-    print("well SOMETHING is running")
     try:
+        distance = sonar.distance
         print((sonar.distance,))
-        AHHH = sonar.distance
+        if distance >= 5 and distance < 20:
+            red = int(simpleio.map_range(distance, 5, 20, 255, 0))
+            blue = int(simpleio.map_range(distance, 5, 20, 0, 255))
+            green = int(simpleio.map_range(distance, 5, 20, 0, 0))
+        if distance >= 20 and distance < 35:
+            red = int(simpleio.map_range(distance, 20, 35, 0, 0))
+            blue = int(simpleio.map_range(distance, 20, 35, 255, 0))
+            green = int(simpleio.map_range(distance, 20, 35, 0, 255))
+        print(red, green, blue)
+        dot.fill((red, green, blue))
     except RuntimeError:
-        print("N/A")
-    if AHHH >= 5 and AHHH < 20:
-        red = simpleio.map_range(distance, 5, 20, 255, 0)
-        blue = simpleio.map_range(distance, 5, 20, 0, 255)
-        green = 0
-    if AHHH >= 20 and AHHH < 35:
-        red = 0
-        blue = simpleio.map_range(distance, 5, 20, 255, 0)
-        green = simpleio.map_range(distance, 5, 20, 0, 255)
-    dot.fill(red, blue, green)
-
+        print("Sensor bad :(")
     time.sleep(0.1)
