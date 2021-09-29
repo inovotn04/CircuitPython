78c07a6743a575b8875ba52488459d85dbf41f4b
Ian Novotne <inovotn04@ccs.local>
2021-09-29 09:45:31 -0400

    Adding photointerrupter code and gif

--- Files/photointerrupterCode.py
+++ Files/photointerrupterCode.py
@@ -1,11 +1,14 @@
 import time
 import board
 import digitalio
+import neopixel
 
 interrupter = digitalio.DigitalInOut(board.D7)
 interrupter.direction = digitalio.Direction.INPUT
 interrupter.pull = digitalio.Pull.UP
 
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+dot.brightness = 0.1
 counter = 0
 
 photo = False
@@ -16,11 +19,12 @@ start = time.time()
 while True:
     photo = interrupter.value
     if photo and not state:
-            counter += 1
+        counter += 1
+        dot.fill((0, 255, 0))
     state = photo
-
     remaining = max - time.time()
 
     if remaining <= 0:
         print("Interrupts:", str(counter))
         max = time.time() + 4
+        dot.fill((170, 0, 0))
